24795cf8198f66b0043f5deaf5c50f9a62075d36
tlyong1992 <tlyong1992@hotmail.com>
2021-02-10 00:07:52 +0800

    配置打包Title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = '唐哼哼博客'
+                return args
+            })
+    }
 }
