700c08ecafa162402f1c570171b1be6c4049aa6d
tlyong1992 <tlyong1992@hotmail.com>
2021-02-25 21:49:59 +0800

    首页文章列表，文章详情页

--- backend/modules/blog/views.py
+++ backend/modules/blog/views.py
@@ -6,5 +6,24 @@ blog = Blueprint('blog', __name__, url_prefix='/')  # news蓝图
 @blog.route('/', methods=['GET'])
 def index():
     """首页"""
-    data = '你好，世界！'
-    return render_template('index.html', data=data)
+    paginate = {
+        'page': 1, 'per_page': 6, 'total': 3,
+    }
+
+    items = [
+        {'title': '文章1', 'category': '分类', 'date': '2021-02-25', 'url': '/2021/0225/测试文章1',
+         'last_modified': '2021-02-25'},
+        {'title': '文章2', 'category': '分类', 'date': '2021-02-25', 'url': '/2021/0225/测试文章2',
+         'last_modified': '2021-02-25'},
+        {'title': '文章3', 'category': '分类', 'date': '2021-02-25', 'url': '/2021/0225/测试文章3',
+         'last_modified': '2021-02-25'}
+    ]
+
+    return render_template('index.html', posts=items, paginate=paginate)
+
+
+@blog.route('/<int:year>/<date>/<title>', methods=['GET'])
+def post(year: str, date: str, title: str):
+    post = {'title': '文章1', 'category': '分类', 'date': '2021-02-25', 'url': '/2021/0225/测试文章1',
+            'last_modified': '2021-02-25', 'html': '阿黄是个变态'}
+    return render_template("post.html", post=post)
